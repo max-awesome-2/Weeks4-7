46a8b37b85b9af583fb48abb020282e4a53ff538
SHERNET\gelinama <gelinama@sheridancollege.ca>
2025-02-14 15:40:07 -0500

    Started working on Tank exercise

---
+++ Assets/Scripts/Coding Gym/Week 6/Tank/TankController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+
+    /*
+     * Psuedocode:
+     * 
+     * vector2 move
+     * set move x to input axis horizontal
+     * translate tank position by move * speed * time.deltatime
+     * 
+     * bind tank position between sides of screen
+     * 
+     * get mouseposition
+     * get world position of mouse
+     * use mathf.atan2 to get angle between tank and mouse
+     * apply angle to tank barrel
+     * 
+     * on mouse click:
+     *  spawn prefab bullet
+     *  access prefab bullet script and pass in the movement direction
+     * 
+     * extra functions to be used by ui settings script:
+     * setmovement, setsize
+     * 
+     * 
+     */
+
+    public float moveSpeed;
+
+    private Vector2 screenBounds;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // calculate and store screen bounds
+        screenBounds = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(0, 0, -Camera.main.transform.position.z)).x, Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, -Camera.main.transform.position.z)).x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 move = new Vector3(Input.GetAxis("Horizontal"), 0);
+
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+
+        
+    }
+}
