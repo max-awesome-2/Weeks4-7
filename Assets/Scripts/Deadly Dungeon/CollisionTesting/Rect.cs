0314e1246af5c28a1151d580f4cedee57e402c70
SHERNET\gelinama <gelinama@sheridancollege.ca>
2025-02-20 15:05:16 -0500

    Added pseudocode for rect class

--- Assets/Scripts/Deadly Dungeon/CollisionTesting/Rect.cs
+++ Assets/Scripts/Deadly Dungeon/CollisionTesting/Rect.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rect : MonoBehaviour
+public class Rect
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   /*
+    * x, y, x2, y2
+    * width / height
+    * 
+    * detect collision (rect)
+    *   if (a.x1 < b.x2 && a.x2 > b.x1 && a.y1 > b.y2 && a.y2 < b.y1)
+    *       collision
+    *       
+    *    vector3 direction = playerposition - transform.position
+    *    
+    *    bool xNegative = direction.x < 0;
+    *    bool yNegative = direction.y < 0;
+    *    
+    *    bool xMagGreater = mathf.Abs(direction.x) > mathf.abs(direction.y);
+    *    
+    *    collisionside = xMagGreater ? (xNegative ? 3 : 1) : (yNegative ? 2 : 0);
+    *    
+    * move(newx, newy)
+    *   set x, y, x2, y2 based on new pos + width and height 
+    *   
+    *   
+    * 
+    */
 }
