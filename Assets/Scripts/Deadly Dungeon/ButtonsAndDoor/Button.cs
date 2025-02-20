4b53251471948cccb2478e7d07842c76d1ecbfd6
James Sutton <suttjame@shernet.sheridancollege.ca>
2025-02-20 15:34:13 -0500

    Added pseudocode for buttons and doors

---
+++ Assets/Scripts/Deadly Dungeon/ButtonsAndDoor/Button.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    /*public boolean to check if button is active
+     * public boolean to check if button is pressed
+     * spriterender of button 
+     * 
+     * 
+     * timer float
+     * public float for timer break
+     * 
+     * assign componenent to sprite render
+     * getcurrent color value
+     * 
+     *if pressed is true (interected with by player)
+     *change color to darker color
+     *timer using delta time to incrementally increase
+     *active is switched
+     *
+     *if statement running when timer reaches break value
+     *set pressed to false
+     *set color back to originial
+     */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
