bdff460c674119d0309c7ee7defe46758bcc6b3b
SHERNET\gelinama <gelinama@sheridancollege.ca>
2025-02-20 15:34:41 -0500

    Added pseudocode for wall and collideable

---
+++ Assets/Scripts/Deadly Dungeon/CollisionTesting/Collideable.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collideable : MonoBehaviour
+{
+
+    /*
+     * parent class for everything with a rect
+     * 
+     * public Rect rect;
+     * 
+     * public void GenerateRect() {
+     *  rect = new Rect(transform.position, transform.localscale);
+     * }
+     */
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
